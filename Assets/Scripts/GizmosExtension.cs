3d84641a3e57cefd5cda4aa4adffc36b2e5ab533
Shubhra <ssuvro22@outlook.com>
2020-11-18 21:14:57 -0800

    Add Basic Gizmo to draw wheel radius on editor

---
+++ Assets/Scripts/GizmosExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmosExtension:MonoBehaviour
+{
+    public static void DrawWireCircle(Vector3 pos, Quaternion rot, float radius, int detail = 32)
+    {
+        
+        Vector3[] points3D = new Vector3[detail];
+
+        for(int i = 0;i<detail; i++)
+        {
+
+            float angle = Mathf.PI * 2 * i / detail;
+
+            float x = radius * (float) Math.Cos(angle);
+            float y = radius * (float) Math.Sin(angle);
+            
+            Vector2 point2D = new Vector2(x, y);
+            
+            points3D[i] = pos + rot * point2D;
+
+        }
+
+        for (int i = 0; i < detail-1; i++)
+        {
+            Gizmos.DrawLine(points3D[i], points3D[i + 1]);
+        }
+        Gizmos.DrawLine(points3D[detail-1], points3D[0]);
+    }
+}
--- Assets/Scripts/WheelCollider.cs
+++ Assets/Scripts/WheelCollider.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WheelCollider : MonoBehaviour
 {
+        public float radius;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            GizmosExtension.DrawWireCircle(transform.position,transform.rotation,radius);
+        }
     }
 }
+
